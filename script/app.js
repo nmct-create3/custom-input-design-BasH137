eeaa0d8da13ea19c9063e154cf3e39bb4378361d
Bas.VM <basvanmas@gmail.com>
2023-09-30 23:25:00 +0200

    Made Password Toggle

--- script/app.js
+++ script/app.js
@@ -1,14 +1,31 @@
+// Define the function to handle the password switcher
 const handlePasswordSwitcher = function () {
-    console.log("handlePasswordSwitcher executed");
-}
-
-const init = function () {
+    const passwordInput = document.querySelector('.js-password-input');
+    const passwordToggleCheckbox = document.querySelector('.js-password-toggle-checkbox');
+  
+    // Add an event listener to the checkbox
+    passwordToggleCheckbox.addEventListener('change', function () {
+      // Check the state of the checkbox
+      if (passwordToggleCheckbox.checked) {
+        // If checked, change the input type to 'text' (show password)
+        passwordInput.type = 'text';
+      } else {
+        // If not checked, change the input type to 'password' (hide password)
+        passwordInput.type = 'password';
+      }
+    });
+  };
+  
+  // Initialize the password switcher
+  const init = function () {
     console.log('Script loaded!');
-
-    // init password switcher
+  
+    // Initialize password switcher
     handlePasswordSwitcher();
-
-    // Other init functions here
-}
-
-init();
+  
+    // Other initialization code here
+  };
+  
+  // Call the init function to start the script
+  init();
+  
